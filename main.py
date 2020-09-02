9391f1bdb393a2b74e24b7dda854df306b3c3eb3
milancomi <38693770+milancomi@users.noreply.github.com>
2020-09-02 21:35:57 +0200

    Update bugs

--- main.py
+++ main.py
@@ -1,76 +1,47 @@
 from kivy.app import App
+from kivy.uix.widget import Widget
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+from kivy.uix.scrollview import ScrollView
 import json
 import requests
 
+class MyDatabase(Widget):
+    textValue = ObjectProperty(None)
 
-KV = Builder.load_string("""
-ScreenManager:
-    id:manager
-    Screen:
-        BoxLayout:
-            orientation: 'vertical'
-            GridLayout:
-                cols:1
-                Label:
-                    text: 'enter JSON'
-                TextInput:
-                    size_hint_y:2
-                    id: JSON
-                    text:'{"Parent":{"Child1":"Value","Child2":"Value"}}'
-            GridLayout:
-                cols:1
-                Button:
-                    text: 'Patch Line'
-                    on_release: app.patch(JSON.text)
-                Button:
-                    text:'Post Line'
-                    on_release: app.post(JSON.text)
-
-                Button:
-                    text:'Put Line'
-                    on_release: app.put(JSON.text)
-                Button:
-                    text:'Delete Line'
-                    on_release: app.delete(JSON.text)
-                Button:
-                    text:'Get & Print Database'
-                    on_release: app.get()
-
-""")
-
-class MyApp(App):
     url = 'https://samplefirebaseapp-13d04.firebaseio.com/.json'
 
-    def patch(self,JSON):  #  CreateOrUpdate
+    def patch(self,instance):  #  CreateOrUpdate
         # {"Parent":{"Child1":"Value","Child2":"Value"}}
-        to_database = json.loads(JSON)
-        requests.patch(url=self.url,json = to_database) 
+        to_database = json.loads(self.textValue.text)
+        requests.patch(url=self.url,json = to_database)
+        # self.textValue = '{"naziv_tabele":{"naziv_podatka1":"vrednost_podatka1","naziv_podatka2":"vrednost_podatka2"}}'
 
-    def post(self,JSON): # ne koristi se, kreira random name
+    def post(self,instance): # ne koristi se, kreira random name
 
-        to_database = json.loads(JSON)
+        to_database = json.loads(self.textValue.text)
         requests.post(url=self.url,json = to_database) 
 
-    def put(self,JSON): # brise sve i ubacuje samo poslednji record
+    def put(self,instance): # brise sve i ubacuje samo poslednji record
 
-        to_database = json.loads(JSON)
+        to_database = json.loads(self.textValue.text)
         requests.put(url=self.url,json = to_database) 
 
-    def delete(self,JSON):
+    def delete(self,instance):
         # Parent/Child1
-        requests.delete(url=self.url[:-5]+ JSON + ".json") 
+        requests.delete(url=self.url[:-5]+ self.textValue.text + ".json") 
 
     auth_key='Ccnw5uXXrTLjVq4ghFSD7nlIp0Ov7MKFYrHWGriH'
 
     def get(self):
         request = requests.get(self.url+'?auth='+self.auth_key)
         data = request.json()
-        
+        self.textValue.text = json.dumps(data, indent=2)
         print(data)
     
+class MyApp(App):
     def build(self):
-        return KV
+        return MyDatabase()
 
 if __name__=='__main__':
     MyApp().run()
